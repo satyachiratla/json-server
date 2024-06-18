13d8d44fe6a0bfc36fa4aa707060d2aebd0bfd38
satyachiratlae <satyachiratla77@gmail.comm>
2024-06-18 17:23:22 +0530

    shifted to commonjs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import jsonServer from "json-server"; // importing json-server library
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-// eslint-disable-next-line no-undef
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);

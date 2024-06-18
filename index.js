eff75f246448bf2552cd4a65e768bf362a0e4862
satyachiratlae <satyachiratla77@gmail.comm>
2024-06-18 17:05:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+import { create, router as _router, defaults } from "json-server"; // importing json-server library
+const server = create();
+const router = _router("db.json");
+const middlewares = defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);

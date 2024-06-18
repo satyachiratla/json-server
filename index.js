810d791ccddbb1c3ef8407e015d34e8f14eeab6f
satyachiratlae <satyachiratla77@gmail.comm>
2024-06-18 17:09:56 +0530

    resolved issue

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-import { create, router as _router, defaults } from "json-server"; // importing json-server library
-const server = create();
-const router = _router("db.json");
-const middlewares = defaults();
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
